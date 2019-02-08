07d0159f0508c48f9549b34c4f4d941e217937af
outawalpa <47415895+outawalpa@users.noreply.github.com>
2019-02-08 09:29:35 +0100

    Ajout inteface/menu pour les choix users (#5)
    Ajout inteface/menu pour les choix users

--- src/imiecity/Monde.java
+++ src/imiecity/Monde.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 public class Monde {
     
-    ArrayList<Lieu> lieux = new ArrayList();
-    ArrayList<Flux> flux = new ArrayList();
+    public static ArrayList<Lieu> lieux = new ArrayList();
+    public static ArrayList<Flux> flux = new ArrayList();
     
     private static Monde instance =null; 
      
@@ -27,24 +27,7 @@ public class Monde {
             nouveauLieu.addObserveur(new ConsoleObserveur());
 
         }
-        for(int i = 0 ; i < 3 ; i++){
 
-            int randOrigine = (int)(Math.random() * (lieux.size()));
-
-            int randDest;
-            do {
-                randDest = (int)(Math.random() * (lieux.size()));
-            } while(randDest == randOrigine);
-
-            Lieu origine= lieux.get(randOrigine);
-            Lieu destination= lieux.get(randDest);
-
-            // ça aussi c'est moche
-            int ressource = (int)(Math.random() * (2));
-
-            flux.add(new Flux(origine, destination, ressource));
-
-        }
     }
     public ArrayList<Processable> getProcessables(){
         ArrayList<Processable> tab = new ArrayList();
--- src/imiecity/Ressource.java
+++ src/imiecity/Ressource.java
@@ -16,7 +16,8 @@ public class Ressource {
     public final static int PLANCHE=1;
     public final static int FER=2;
     public final static int CHARBON=3;
-    public final static String TAB[]=  {"Arbre", "Planche", "Fer", "Charbon"};
+    public final static int OUVRIER=4; // TOUJOURS EN DERNIER
+    public final static String TAB[]=  {"Arbre", "Planche", "Fer", "Charbon", "Ouvrier"};
     
     public static String nom(int ressourceId){
         return TAB[ressourceId];
