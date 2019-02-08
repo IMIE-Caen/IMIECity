b235536c27ec9fe8b25c806e290d843bb565b765
NotXahurt <47415991+NotXahurt@users.noreply.github.com>
2019-02-08 09:30:56 +0100

    Add files via upload

--- src/imiecity/Ressource.java
+++ src/imiecity/Ressource.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package imiecity;
 
-
-/**
- *
- * @author clement
- */
 public class Ressource {
     public final static int ARBRE=0;
     public final static int PLANCHE=1;
-    public final static String TAB[]=  {"Arbre", "Planche"};
+    public final static int FER=2;
+    public final static int CHARBON=3;
+    public final static int ACIER=4;
+    public final static String TAB[]=  {"Arbre", "Planche","Fer","Charbon","Acier"};
     
     public static String nom(int ressourceId){
         return TAB[ressourceId];
